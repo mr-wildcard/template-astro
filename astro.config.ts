cfa4af25393e3c347c1390af64d588dbf88883e8
Julien Viala <julien.viala@shine.fr>
2024-01-19 15:11:49 +0100

    chore(prettier): install

--- astro.config.ts
+++ astro.config.ts
@@ -1,31 +1,34 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import yaml from "@rollup/plugin-yaml";
 
 const LOCAL_PORT = 4321;
 const LOCAL_HOST = "localhost";
 
 function getSiteURL() {
-	if (process.env.CF_PAGES) {
-		if (process.env.CF_PAGES_BRANCH === "main") {
-			return "https://team-ux-com.pages.dev/";
-		} else {
-			return `https://${process.env.CF_PAGES_BRANCH}.team-ux-com.pages.dev/`;
-		}
-	} else if (process.env.GITHUB_ACTION) {
-		return "https://www.team-ux.com/";
-	} else {
-		return `http://${LOCAL_HOST}:${LOCAL_PORT}/`;
-	}
+  if (process.env.CF_PAGES) {
+    if (process.env.CF_PAGES_BRANCH === "main") {
+      return "https://team-ux-com.pages.dev/";
+    } else {
+      return `https://${process.env.CF_PAGES_BRANCH}.team-ux-com.pages.dev/`;
+    }
+  } else if (process.env.GITHUB_ACTION) {
+    return "https://www.team-ux.com/";
+  } else {
+    return `http://${LOCAL_HOST}:${LOCAL_PORT}/`;
+  }
 }
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon(), tailwind(), sitemap()],site: getSiteURL(),
+  integrations: [icon(), tailwind(), sitemap()],
+  site: getSiteURL(),
   vite: {
+    plugins: [yaml()],
     build: {
-      // mainly for transpiling optional chaining for iOS 12, ffs
+      // mainly for transpiling optional chaining for iOS 12
       target: "es2019",
     },
   },
