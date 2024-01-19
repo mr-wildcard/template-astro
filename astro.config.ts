5c93b45c527247e119a9a6ea0a8b9db60efe9c15
Julien Viala <julien.viala@shine.fr>
2024-01-19 15:02:07 +0100

    refactor: sn image location

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const LOCAL_PORT = 4321;
+const LOCAL_HOST = "localhost";
+
+function getSiteURL() {
+	if (process.env.CF_PAGES) {
+		if (process.env.CF_PAGES_BRANCH === "main") {
+			return "https://team-ux-com.pages.dev/";
+		} else {
+			return `https://${process.env.CF_PAGES_BRANCH}.team-ux-com.pages.dev/`;
+		}
+	} else if (process.env.GITHUB_ACTION) {
+		return "https://www.team-ux.com/";
+	} else {
+		return `http://${LOCAL_HOST}:${LOCAL_PORT}/`;
+	}
+}
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
