60627fc8b1b542f2e75c9df2c23f9dfd39b2b89a
Julien Viala <julien.viala@shine.fr>
2024-04-16 16:01:27 +0200

    test: screenshot specific element

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 test("Snapshot homepage", async ({ page }) => {
   await page.goto("/");
 
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page.getByRole("list")).toHaveScreenshot();
 });
