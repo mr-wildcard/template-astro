aee47cce5cb2c03253d786ca2320c152605c0129
Julien Viala <julien.viala@shine.fr>
2024-04-16 16:27:47 +0200

    test: wait for font loading

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from "@playwright/test";
 test("Snapshot homepage", async ({ page }) => {
   await page.goto("/");
 
+  await page.waitForFunction(() => document.fonts.ready);
+
   await expect(page.getByRole("list")).toHaveScreenshot();
 });
